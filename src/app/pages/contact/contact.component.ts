b6b68be7a7015868643a3125b90adf29c1589451
Yuji <mallarilevin@gmail.com>
2025-02-28 21:07:59 +0800

    + routing

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
